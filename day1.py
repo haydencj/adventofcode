bbbd87b7e9fefc496387084d1d7e25ea81922dc7
haydencj <haydenjohns02@gmail.com>
2023-12-17 02:45:21 -0600

    day1 part2 solved

--- day1.py
+++ day1.py
@@ -1,40 +1,43 @@
-def locateValue(text: str) -> int:
-    nums = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9 }
-    w = ''
-    value = []
-    
-    for x in text:
-        if w in nums:
-            value.append(str(nums[w]))
-            break
-        elif x.isdigit() == True:
-            value.append(x)
-            break
-        else:
-            w = w + x
+from sys import argv
 
-    w = ''
+nums = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9 }
 
-    for y in text[::-1]:
-        if w.strip() in nums:
-            value.append(str(nums[w.strip()]))
-            break
-        if y.isdigit() == True:
-            value.append(y)
-            break
+# find first digit
+def checkFront(text: str) -> str:
+    w = '' # string to check substrings of
+
+    for i in text:
+        for j in range(len(w)): 
+            if w[-j:] in nums: # check every substring for digit
+                return str(nums[w[-j:]])
+
+        if i.isdigit() == True: # check if current character is digit
+            return i
         else:
-            w = y + w 
-    
-    print(value)
-    
-    return int(value[0] + value[1])
+            w = w + i # add character to string
+
+# find last digit
+def checkBack(text: str) -> str:
+    w = '' # string to check substrings of
+
+    for i in text[::-1]:
+        for j in range(len(w)):
+            if w[:j] in nums: # check every substring for digit
+                return str(nums[w[:j]])
+            
+        if i.isdigit() == True: # check if current character is digit
+            return i
+        else: 
+            w = i + w # add character to string
 
 if __name__ == '__main__':
+    cat = list()
     values = list()
 
-    with open("test.txt", "r") as file:
+    with open(argv[1], "r") as file:
         for line in file:
-            values.append(locateValue(line))
+            value = checkFront(line) + checkBack(line) # concatenate first and last digits to form two digit
+            values.append(int(value)) # add value to list to be summed
 
         calibrationSum = sum(values)
         print(calibrationSum)
